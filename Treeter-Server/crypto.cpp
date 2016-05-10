9dea937546e064bbd7c4d0959284d509b870cf6d
psrok1 <psrok1@gmail.com>
2016-05-10 19:52:08 +0200

    Crypto::AES/RSA complete implementation

--- Treeter-Server/crypto.cpp
+++ Treeter-Server/crypto.cpp
@@ -15,56 +15,141 @@
 
 namespace Crypto
 {
+    typedef std::shared_ptr<RSA> RSAPtr;
+
+    RSAPtr createRSAKey()
+    {
+        BIGNUM* e = BN_new();
+        RSAPtr key(RSA_new(), RSA_free);
+
+        BN_set_word(e, 65537);
+        RSA_generate_key_ex(key.get(), 1024, e, nullptr);
+
+        BN_free(e);
+
+        return key;
+    }
+
+    AESContext::AESContext(unsigned char* k): key(k) { }
+
+    AESContext::~AESContext()
+    {
+        delete [] key;
+    }
+
+    std::string AESContext::encrypt(std::string msg)
+    {
+        EVP_CIPHER_CTX *ctx;
+        int len;
+        const char *plaintext = msg.data();
+        int plaintext_len = msg.size();
+
+        unsigned char *ciphertext = new unsigned char[plaintext_len+256];
+        int ciphertext_len;
+
+        ctx = EVP_CIPHER_CTX_new();
+
+        EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv);
+        EVP_EncryptUpdate(ctx, ciphertext, &len, reinterpret_cast<const unsigned char*>(plaintext), plaintext_len);
+        ciphertext_len = len;
+        EVP_EncryptFinal_ex(ctx, ciphertext + len, &len);
+        ciphertext_len += len;
+
+        EVP_CIPHER_CTX_free(ctx);
+
+        std::string output(reinterpret_cast<char*>(ciphertext), ciphertext_len);
+
+        delete [] ciphertext;
+        return output;
+    }
+
+    std::string AESContext::decrypt(std::string msg)
+    {
+        EVP_CIPHER_CTX *ctx;
+        int len;
+        const char *ciphertext = msg.data();
+        int ciphertext_len = msg.size();
+
+        unsigned char *plaintext = new unsigned char[ciphertext_len];
+        int plaintext_len;
+
+        ctx = EVP_CIPHER_CTX_new();
+
+        EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv);
+        EVP_DecryptUpdate(ctx, plaintext, &len, reinterpret_cast<const unsigned char*>(ciphertext), ciphertext_len);
+        plaintext_len = len;
+        EVP_DecryptFinal_ex(ctx, plaintext + len, &len);
+        plaintext_len += len;
+
+        EVP_CIPHER_CTX_free(ctx);
+
+        std::string output(reinterpret_cast<char*>(plaintext), plaintext_len);
+
+        delete [] plaintext;
+        return output;
+    }
+
+    unsigned char AESContext::iv[16] = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0";
+
+
+    RSAContext::RSAContext(RSAPtr k): key(k) { }
+
+    std::string RSAContext::getEncodedPublicKey()
+    {
+        BIO* bio = BIO_new(BIO_s_mem());
+
+        PEM_write_bio_RSAPublicKey(bio, key.get());
+
+        size_t skey_len = BIO_ctrl_pending(bio);
+        char* skey_buf = new char[skey_len];
+        BIO_read(bio, (void*)skey_buf, skey_len);
+
+        std::string serialized = Base64::encode(std::string(skey_buf, skey_len));
+
+        BIO_free(bio);
+        delete [] skey_buf;
+        return serialized;
+    }
+
+    AESContext RSAContext::decodeAESKey(std::string aes_key)
+    {
+        std::string raw_encoded_aes = Base64::decode(aes_key);
+        unsigned char* raw_aes = new unsigned char[raw_encoded_aes.length()];
+
+        RSA_private_decrypt(raw_encoded_aes.size(),
+                            reinterpret_cast<const unsigned char*>(raw_encoded_aes.data()),
+                            raw_aes, key.get(), RSA_PKCS1_PADDING);
+
+        return AESContext(raw_aes);
+    }
+
     namespace RSAProvider
     {
-        RSA* rsa_key = nullptr;
+        RSAPtr currentKey;
         std::mutex mu;
 
-        void set(RSA* new_key)
+        void _set(RSAPtr key)
         {
             std::unique_lock<std::mutex> lck(mu);
-            if(rsa_key)
-                RSA_free(rsa_key);
-            rsa_key = new_key;
+            currentKey = key;
         }
 
-        RSA* get()
+        RSAPtr _get()
         {
             std::unique_lock<std::mutex> lck(mu);
-            assert(rsa_key!=nullptr);
-            return rsa_key;
+            assert(currentKey!=nullptr);
+            return currentKey;
         }
 
         void generate()
         {
-            BIGNUM* e = BN_new();
-            RSA* key = RSA_new();
-
-            BN_set_word(e, 65537);
-            RSA_generate_key_ex(key, 1024, e, nullptr);
-
-            BN_free(e);
-
-            set(key);
+            RSAPtr newKey = createRSAKey();
+            _set(newKey);
         }
 
-        std::string getPublicKey()
+        RSAContext getKey()
         {
-            RSA* key = get();
-            BIO* bio = BIO_new(BIO_s_mem());
-
-            PEM_write_bio_RSAPublicKey(bio, key);
-
-            size_t skey_len = BIO_ctrl_pending(bio);
-            char* skey_buf = new char[skey_len];
-            BIO_read(bio, (void*)skey_buf, skey_len);
-
-            std::cout << skey_len << "\n";
-            std::string serialized = Base64::encode(std::string(skey_buf, skey_len));
-
-            BIO_free(bio);
-            delete [] skey_buf;
-            return serialized;
+            return RSAContext(_get());
         }
     }
 
@@ -98,38 +183,9 @@ namespace Crypto
         delete [] locks;
     }
 
-    std::string messageEncrypt(std::string msg, AES_KEY key)
-    {
-//        EVP_CIPHER_CTX *ctx;
-
-//        int len;
-
-//        int ciphertext_len;
-
-//        ctx = EVP_CIPHER_CTX_new();
-//        EVP_EncryptInit(&ctx, EVP_aes_256_cbc(), key, iv);
-//        EVP_EncryptUpdate(&ctx, out, &outlen1, in, sizeof(in));
-//        EVP_EncryptFinal(&ctx, out + outlen1, &outlen2);
-
-//        /* Clean up */
-//        EVP_CIPHER_CTX_free(ctx);
-
-//        return ciphertext_len;
-    }
-
-    std::string messageDecrypt(std::string msg, AES_KEY key)
-    {
-
-    }
-
-    AES_KEY keyDecrypt(std::string key, RSA* keypair)
-    {
-
-    }
-
     void test()
     {
         RSAProvider::generate();
-        std::cout << RSAProvider::getPublicKey() << "\n";
+        std::cout << RSAProvider::getKey().getEncodedPublicKey() << "\n";
     }
 }
--- Treeter-Server/crypto.h
+++ Treeter-Server/crypto.h
@@ -2,18 +2,43 @@
 #define CRYPTO_H
 
 #include <string>
+#include <memory>
 #include "openssl/rsa.h"
 #include "openssl/aes.h"
 
 namespace Crypto
 {
+    typedef std::shared_ptr<RSA> RSAPtr;
+
+    class AESContext
+    {
+        unsigned char* key;
+        static unsigned char iv[16];
+    public:
+        AESContext(unsigned char* k);
+        ~AESContext();
+        std::string encrypt(std::string msg);
+        std::string decrypt(std::string msg);
+    };
+
+    class RSAContext
+    {
+        RSAPtr key;
+    public:
+        RSAContext(RSAPtr k);
+        std::string getEncodedPublicKey();
+        AESContext decodeAESKey(std::string aes_key);
+    };
+
+    namespace RSAProvider
+    {
+        extern void generate();
+        extern RSAContext getKey();
+    }
+
     extern void initialize();
     extern void test();
     extern void free();
-
-    extern std::string messageEncrypt(std::string msg, AES_KEY key);
-    extern std::string messageDecrypt(std::string msg, AES_KEY key);
-    extern AES_KEY keyDecrypt(std::string key, RSA* keypair);
 }
 
 #endif // CRYPTO_H
