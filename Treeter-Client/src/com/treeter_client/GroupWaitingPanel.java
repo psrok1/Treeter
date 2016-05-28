799801783655d274f01df6d064b34285f9f17480
martarzy <marta.bandarzy@gmail.com>
2016-05-28 14:50:40 +0200

    GroupWaitingPanel

--- Treeter-Client/src/com/treeter_client/GroupWaitingPanel.java
+++ Treeter-Client/src/com/treeter_client/GroupWaitingPanel.java
@@ -7,11 +7,35 @@ public class GroupWaitingPanel extends JPanel
 {
     private ImageIcon icon;
     JLabel iconLabel;
+    JLabel textLabel;
+    JPanel centerPane;
 
     public GroupWaitingPanel()
     {
+        this.setLayout(new GridBagLayout());
+        centerPane = new JPanel();
+        centerPane.setBackground(new Color(0xA6, 0x80, 0xB8));
+        centerPane.setLayout(new BoxLayout(centerPane, BoxLayout.Y_AXIS));
+        this.setBackground(new Color(0xA6, 0x80, 0xB8));
+
         icon = new ImageIcon(getClass().getResource("/refresh.png"));
         iconLabel = new JLabel(icon);
-        this.add(iconLabel, BorderLayout.SOUTH);
+        textLabel = new JLabel("Komunikat...");
+        textLabel.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 13));
+        textLabel.setForeground(Color.white);
+        textLabel.setBorder(BorderFactory.createEmptyBorder(30, 0, 0, 0));
+
+        iconLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        centerPane.add(iconLabel);
+
+        textLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        centerPane.add(textLabel);
+
+        this.add(centerPane);
+    }
+
+    public void setText(String text)
+    {
+
     }
 }
