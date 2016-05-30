f2167f8ac3eed6d4d2a25f38947ac81fb51697ab
psrok1 <psrok1@gmail.com>
2016-05-30 02:13:18 +0200

    Server: content, not message!

--- Treeter-Server/model/groupmessage.cpp
+++ Treeter-Server/model/groupmessage.cpp
@@ -5,7 +5,7 @@ const nlohmann::json Model::GroupMessage::toJSON() const
 {
     nlohmann::json j;
     j["author"] = author;
-    j["message"] = message;
+    j["content"] = message;
     j["timestamp"] = std::chrono::system_clock::to_time_t(timestamp);
     return j;
 }
